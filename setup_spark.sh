4f88ea2fcd2da7fee6ac513f8f3563ca252afc37
Michael Salwen <onemodtwo@users.noreply.github.com>
2018-03-09 00:59:41 -0500

    Small edits

--- setup_spark.sh
+++ setup_spark.sh
@@ -7,6 +7,8 @@ else
   if [ ! -d $HOME/.ipython/profile_pyspark${1} ]; then
     # create ipython profile in ~/.ipython
     ipython profile create pyspark${1}
+  fi
+  if [ ! -f $HOME/.ipython/profile_pyspark${1}/startup/00-pyspark${1}-setup.py ]; then
     # write python startup script into profile startup directory
     PROFILE_FP=$HOME/.ipython/profile_pyspark${1}/startup/00-pyspark${1}-setup.py 
     printf "import os\n" > $PROFILE_FP
@@ -19,10 +21,10 @@ else
     printf "sys.path.insert(0, os.path.join(spark_home, 'python'))\n" >> $PROFILE_FP
     printf "sys.path.insert(0, os.path.join(spark_home, 'python/lib/', py4j_version))\n" >> $PROFILE_FP
     printf "exec(open(os.path.join(spark_home, 'python/pyspark/shell.py')).read())\n" >> $PROFILE_FP
+  fi
+  if [ ! -d $CONDA_ROOT/kernels/pyspark${1} ]; then
     # write kernel spec to kernel directory in conda root directory
-    KERNEL_DIR=$CONDA_ROOT/kernels/pyspark${1}
     mkdir -p $CONDA_ROOT/kernels/pyspark${1}
-    KERNEL=$KERNEL_DIR/kernel.json
     printf '{
      "argv": [
         "'${CONDA_PREFIX}'/bin/python",
@@ -39,7 +41,7 @@ else
         "CAPTURE_STANDARD_ERR": "true",
         "SEND_EMPTY_OUTPUT": "false",
         "SPARK_HOME": "'${HOME}'/bin/spark/spark'${1}'"
-     }\n}\n' > $KERNEL
+     }\n}\n' > $CONDA_ROOT/kernels/pyspark${1}/kernel.json
   fi
   PREFIX=$CONDA_PREFIX/etc/conda
   # make required directories
@@ -48,8 +50,8 @@ else
   mkdir -p $CONDA_PREFIX/share/jupyter/kernels/pyspark${1}
   ln -s $KERNEL $CONDA_PREFIX/share/jupyter/kernels/pyspark${1}/kernel.json
   # create activation script
-  echo 'export PATH=$HOME/bin/spark/spark'${1}'/bin:$PATH' >> $PREFIX/activate.d/set_env_vars.sh
+  echo 'export PATH=$HOME/bin/spark/spark'${1}'/bin:$PATH' >> $PREFIX/activate.d/set_path.sh
   # create deactivation script
-  echo 'export PATH=$CONDA_PREFIX/bin:$CONDA_PATH_BACKUP' >> $PREFIX/deactivate.d/unset_env_vars.sh
-  unset PROFILE_FP PREFIX KERNEL_DIR KERNEL
+  echo 'export PATH=$CONDA_PREFIX/bin:$CONDA_PATH_BACKUP' >> $PREFIX/deactivate.d/unset_path.sh
+  unset PROFILE_FP PREFIX
 fi
